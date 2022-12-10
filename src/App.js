26e6c3fc5466ae40048093a2deea935ab8a48a67
Squaredius <52355024+Haborym99@users.noreply.github.com>
2022-12-10 02:35:36 +0100

    Filter map & names done, now rendering

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import { useState } from "react";
 import { nanoid } from "nanoid";
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
